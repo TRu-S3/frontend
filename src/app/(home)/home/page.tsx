a7d04020baa229f125371a5490f0993dcce47a21
Sea10wood <miyusea5@gmail.com>
2025-06-27 00:43:42 +0900

    fix:4等分レイアウト実装・サイドバーとフッターの重なり修正

--- src/app/(home)/home/page.tsx
+++ src/app/(home)/home/page.tsx
@@ -18,7 +18,9 @@ export default async function Page() {
         className='flex-1 flex flex-row bg-slate-200 min-h-0 px-[32px]'
         style={{ minHeight: 'calc(100vh - 56px)' }}
       >
-        <SidebarLeft />
+        <div className='w-1/4 min-w-[180px] max-w-[340px] flex-shrink-0'>
+          <SidebarLeft />
+        </div>
         {/* メインコンテンツ：2/4 */}
         <div className='flex-1 min-w-[240px] bg-white'>
           <MainContent />
